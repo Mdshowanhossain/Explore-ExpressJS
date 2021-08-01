5bd1fe72543f7fedc71cb2862b14f485e064af5c
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-08-01 23:20:15 +0600

    Explore-Express-JS

--- ResponeseCookie.js
+++ ResponeseCookie.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// Response Redirect
+// Response Cookie
 
 app.get('/', (req, res) => {
     res.status(200);
@@ -10,14 +10,13 @@ app.get('/', (req, res) => {
     res.end()
 })
 
-app.post('/clearCookie', (req, res) => {
+app.post('/cookie', (req, res) => {
     res.status(200);
-    res.clearCookie('name');
-    res.clearCookie('roll');
-    res.clearCookie('city');
-    res.end('Clear Your Cookie!');
+    res.cookie("name", "sohan");
+    res.cookie("city", "khulna");
+    res.cookie("roll", "44");
+    res.end('Set Your Cookie');
 })
-
 
 app.listen(8000, () => {
     console.log('I am listening on 8000')
--- ResponseClearCookie.js
+++ ResponseClearCookie.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// Response Redirect
+// Response ClearCookie
 
 app.get('/', (req, res) => {
     res.status(200);
