513c1089c51195cb6cbb608f8c0a9b280e809356
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-08-01 23:29:16 +0600

    Explore-Express-JS

--- WorkWithRequest/RequestQuery.js
+++ WorkWithRequest/RequestQuery.js
@@ -3,23 +3,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-
     res.status(200);
     res.send('Welcome to Home Page');
     res.end();
 })
 
 app.get('/query', (req, res) => {
-
     const firstQuery = req.query.first
     const secondQuery = req.query.second
     res.send(firstQuery + " " + secondQuery);
     res.end();
 
 })
-
-
-
 app.listen(8000, () => {
     console.log('I am listening on 8000')
 })
